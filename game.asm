19c8905d855ee8e9a14691392b88fe880bd94700
samborges0910 <sriborges0910@gmail.com>
2025-11-13 23:21:43 -0500

    Test

---
+++ game.asm
@@ -0,0 +1,19 @@
+INCLUDE Irvine32.inc
+
+; Test on commiting code to the repo
+
+.data
+	msg BYTE "Hello", 0
+	string DWORD 0
+
+.code
+	main PROC
+	mov edx, OFFSET msg
+	mov eax, edx
+	call WriteString
+	call ReadChar
+	exit
+
+main ENDP
+
+END main
