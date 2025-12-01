6a494da128c9e6bcc316f4192221c3eded8909ce
samborges0910 <sriborges0910@gmail.com>
2025-11-30 21:14:11 -0500

    Adding function to insert discs

--- game.asm
+++ game.asm
@@ -3,7 +3,7 @@ INCLUDE Irvine32.inc
 .data
 	grid BYTE 42 DUP('.')
 	promptMessage BYTE "Enter a column 1 to 7: ", 0
-	rowString BYTE "| . . . . . . . |", 0 
+	rowString BYTE "| . . . . . . . |", 0
 
 .code
 
@@ -32,16 +32,42 @@ printColumn:
 
 printGrid ENDP
 
+; --- dropping disc function ---
+dropDisc PROC
+  	  dec eax
+  	  mov ebx, eax
+ 	   mov edx, 5 ; disc will be inserted at bottom row
+
+chosenSpot: ; inserting disc at desired column
+   	 mov esi, OFFSET grid ; pointer to start of grid
+   	 mov eax, edx
+   	 imul eax, 7 ; multiply row index by number of columns
+   	 add eax, ebx
+   	 add esi, eax ; point to grid[row][col]
+    	cmp BYTE PTR [esi], '.'
+   	 je placeO ; adds 'O' in empty spot
+   	 ret
+
+placeO:
+   	 mov BYTE PTR [esi], 'O'
+   	 ret
+
+dropDisc ENDP
+
 ; --- main ---
 main PROC
 
 mainLoop: ; implement main loop to prompt input
 	call printGrid
 	call Crlf
-	mov edx, OFFSET promptMessage
+	mov edx, OFFSET promptMessage ; asks for user input
 	call WriteString
 	call ReadInt
-	jmp mainLoop
+
+    	cmp eax, 1
+   	push eax
+    	call dropDisc
+    	jmp mainLoop
 
 main ENDP
 
