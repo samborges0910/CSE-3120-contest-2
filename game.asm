1d386cb5b8a1dcf3002fda5514d553357ef8f2ae
samborges0910 <sriborges0910@gmail.com>
2025-11-30 20:31:00 -0500

    changing code for input

--- game.asm
+++ game.asm
@@ -4,28 +4,45 @@ INCLUDE Irvine32.inc
 	grid BYTE 42 DUP('.')
 	promptMessage BYTE "Enter a column 1 to 7: ", 0
 	rowString BYTE "| . . . . . . . |", 0 
+
 .code
 
+;---printing grid---
 printGrid PROC ; function for printing the grid
-	mov ecx, 6	; amount of rows
-	mov ebx, 0	; index counter
+	mov esi, OFFSET grid
+	mov ecx, 6
+
+printRow: ; added loops to print rows and columns
+	mov edi, OFFSET rowString
+	add edi, 2
+	mov edx, 7
+
+printColumn:
+	mov al, [esi]
+	mov [edi], al
+	add edi, 2
+	inc esi
+	dec edx
+	jnz printColumn
+ 	mov edx, OFFSET rowString
+	call WriteString
+	call Crlf
+	loop printRow
+	ret
 
 printGrid ENDP
 
+; --- main ---
 main PROC
-	mov ecx, 6
-printRows:
-	mov edx, OFFSET rowString
-	call WriteString
-	call Crlf
-	loop printRows
 
 mainLoop: ; implement main loop to prompt input
+	call printGrid
+	call Crlf
 	mov edx, OFFSET promptMessage
 	call WriteString
 	call ReadInt
 	jmp mainLoop
 
-	exit
 main ENDP
+
 END main
