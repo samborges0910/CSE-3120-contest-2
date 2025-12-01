799bb1ab21fdc6995fc099808aeb70a33d21b1b0
samborges0910 <sriborges0910@gmail.com>
2025-11-30 19:34:10 -0500

    implementing a function to print the grid

--- game.asm
+++ game.asm
@@ -5,6 +5,13 @@ INCLUDE Irvine32.inc
 	promptMessage BYTE "Enter a column 1 to 7: ", 0
 	rowString BYTE "| . . . . . . . |", 0 
 .code
+
+printGrid PROC ; function for printing the grid
+	mov ecx, 6	; amount of rows
+	mov ebx, 0	; index counter
+
+printGrid ENDP
+
 main PROC
 	mov ecx, 6
 printRows:
